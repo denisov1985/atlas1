176e403222c9bd333040a39249025ef567e50c25
Dmytro_Denysov <Dmytro.Denysov@racingpost.com>
2017-12-13 15:34:39 +0200

    Initial commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmytro_Denysov
+ * Date: 12/13/2017
+ * Time: 11:39 AM
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends Controller
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function homePage()
+    {
+        return $this->render("page/home-page.html.twig");
+    }
+}

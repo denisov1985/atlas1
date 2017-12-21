625a72441fa798f9d9067a5724a968be3117942e
denysov <dmytro@denysov.net>
2017-12-21 11:54:10 +0200

    Images grabber works

--- src/Migrations/Version20171220152224.php
+++ src/Migrations/Version20171220152224.php
@@ -18,7 +18,6 @@ class Version20171220152224 extends AbstractMigration
         $this->addSql('CREATE TABLE product_image (product_id INT NOT NULL, image_id INT NOT NULL, INDEX IDX_64617F034584665A (product_id), INDEX IDX_64617F033DA5256D (image_id), PRIMARY KEY(product_id, image_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE product_image ADD CONSTRAINT FK_64617F034584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE product_image ADD CONSTRAINT FK_64617F033DA5256D FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE');
-        $this->addSql('DROP TABLE image_product');
     }
 
     public function down(Schema $schema)
@@ -26,9 +25,6 @@ class Version20171220152224 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE image_product (image_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_465A53713DA5256D (image_id), INDEX IDX_465A53714584665A (product_id), PRIMARY KEY(image_id, product_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE image_product ADD CONSTRAINT FK_465A53713DA5256D FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE image_product ADD CONSTRAINT FK_465A53714584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
         $this->addSql('DROP TABLE product_image');
     }
 }
--- src/Migrations/Version20171220163045.php
+++ src/Migrations/Version20171220163045.php
@@ -18,8 +18,6 @@ class Version20171220163045 extends AbstractMigration
         $this->addSql('ALTER TABLE product ADD cover_image_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADE5A0E336 FOREIGN KEY (cover_image_id) REFERENCES image (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D34A04ADE5A0E336 ON product (cover_image_id)');
-        $this->addSql('ALTER TABLE product_image RENAME INDEX product_id TO IDX_64617F034584665A');
-        $this->addSql('ALTER TABLE product_image RENAME INDEX image_id TO IDX_64617F033DA5256D');
     }
 
     public function down(Schema $schema)
@@ -30,7 +28,5 @@ class Version20171220163045 extends AbstractMigration
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADE5A0E336');
         $this->addSql('DROP INDEX UNIQ_D34A04ADE5A0E336 ON product');
         $this->addSql('ALTER TABLE product DROP cover_image_id');
-        $this->addSql('ALTER TABLE product_image RENAME INDEX idx_64617f034584665a TO product_id');
-        $this->addSql('ALTER TABLE product_image RENAME INDEX idx_64617f033da5256d TO image_id');
     }
 }
